7e4d3804d0c9063bfad2ef7b4b1ae82a82f936e7
Anish Paleja <anipaleja@gmail.com>
2025-06-27 14:32:48 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-#!/usr/bin/env bash
-
-# Use python 3.10 if it's available (Render often ignores runtime.txt)
-python3.10 -m venv venv
+#!/bin/bash
+python3 -m venv venv
 source venv/bin/activate
-
-# Upgrade pip and install packages
 pip install --upgrade pip
 pip install -r requirements.txt
